fc5dbf47ddb49500f97e5c3e1d0786f77abe12e0
xiaoming <664374295@qq.com>
2018-08-15 08:58:07 +0800

    数据库调整

--- README.md
+++
@@ -1,2 +0,0 @@
-# django_blog
-Django博客系统
--- blog_django/settings.py
+++ blog_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ja@!p79%-98dpjipr(p(!bc!0uyf(l&+=d4u+v=lpj@_7aizk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False  # True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.dmfly.xin']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.dmfly.xin']
 
 
 # Application definition
@@ -123,4 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
