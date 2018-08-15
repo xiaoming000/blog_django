2a8451f6719d19be13480e1882c3d8e099a73553
xiaoming <664374295@qq.com>
2018-08-15 10:21:15 +0800

    自动生成摘要

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import markdown
+from django.utils.html import strip_tags
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -37,6 +39,22 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = '    '+strip_tags(md.convert(self.body))[:80]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_time']
--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,9 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
+    post.increase_views()   # 阅读量
+
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
