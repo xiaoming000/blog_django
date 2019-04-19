8c90bda7988f33287b3774e113698f384613d459
xiaoming <664374295@qq.com>
2019-04-19 09:54:27 +0800

    删除了env

--- blog_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!f:\desktop\python\python~1\django\blog_d~1\blog_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
